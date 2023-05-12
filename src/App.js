a8c4cbc2ec4ddd730b801e52f2b808465426c11d
Krystian Żybura <Krystian12324444@gmail.com>
2023-05-12 15:58:41 +0200

    removing unnecessary main component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import Form from "./Form";
-import Main from "./Main";
 import Container from "./Container";
 
 function App() {
   return (
     <Container>
-      <Main
-        body={
           <Form
             legend={"Kantor"}
             specialText={"------->"}
           />
-        }
-      />
     </Container>
   );
 };
--- src/Main/index.js
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-
-const Main = ({ body }) => (
-    <main className="main">
-        {body}
-    </main>
-);
-
-export default Main;
