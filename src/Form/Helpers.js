f45870688604d77092bccf8af5d4bfa78a30e21a
Krystian Żybura <Krystian12324444@gmail.com>
2023-05-16 11:52:35 +0200

    adding new available currency (GBP)

--- src/Form/Helpers.js
+++ src/Form/Helpers.js
@@ -2,12 +2,16 @@ const currencies = [
   { name: "PLN", mark: "zł" },
   { name: "USD", mark: "$" },
   { name: "EUR", mark: "€" },
+  { name: "GBP", mark: "£" },
 ];
 
 const currencyPairsData = [
   { pair: "EUR/PLN", exchangeRate: 4.61 },
   { pair: "USD/PLN", exchangeRate: 4.2 },
   { pair: "EUR/USD", exchangeRate: 1.09 },
+  { pair: "GBP/PLN", exchangeRate: 5.18 },
+  { pair: "GBP/EUR", exchangeRate: 1.15 },
+  { pair: "GBP/USD", exchangeRate: 1.25 },
 ];
 
 const calculateResult = (amount, currencyPair, currencyExchange) => {
