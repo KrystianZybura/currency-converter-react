9462c2ea01b6aea7d92cb04902da5a10990f80c9
Krystian Żybura <Krystian12324444@gmail.com>
2023-05-17 22:04:27 +0200

    custom polish clock added (test)

--- src/App/Form/Clock/index.js
+++ src/App/Form/Clock/index.js
@@ -2,13 +2,31 @@ import { useState } from "react";
 import "./style.css";
 
 const Clock = () => {
-  const [localDate, setLocalDate] = useState(new Date());
+  const [localDate, setLocalDate] = useState(
+    new Date().toLocaleString("pl", {
+      weekday: "long",
+      day: "numeric",
+      month: "long",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    })
+  );
 
   setInterval(() => {
-    setLocalDate(new Date());
+    setLocalDate(
+      new Date().toLocaleString("pl", {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+      })
+    );
   }, 1000);
 
-  return <span className="form__clock">{`${localDate}`}</span>;
+  return <span className="form__clock">{`Dzisiaj jest ${localDate}`}</span>;
 };
 
 export default Clock;
