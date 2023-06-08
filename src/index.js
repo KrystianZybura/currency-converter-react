ca7b974839f6dda62a0d2bc70e520904f2a5a81e
Krystian Żybura <Krystian12324444@gmail.com>
2023-06-08 22:32:14 +0200

    correcting import due to previous change

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import GlobalStyles from "./GlobalStyles";
 import { ThemeProvider } from "styled-components";
-import { theme } from "./Theme";
+import { theme } from "./theme";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
