b5ad6a0151a29cd2ed2f923110457f30a57e920f
Krystian Żybura <Krystian12324444@gmail.com>
2023-06-18 19:10:38 +0200

    changing "success" status outcome

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -43,58 +43,60 @@ const Form = ({ legend, specialText }) => {
         {status === "loading" && <LoadingAnimation />}
         {status === "error" && <ErrorMessage />}
         {status === "success" && (
-          <Wrapper>
+          <>
+            <Wrapper>
+              <label>
+                <Select
+                  value={inputCurrency}
+                  onChange={(event) => onCurrencyChange(event, "input")}
+                >
+                  {currencyKeys.map((name) => (
+                    <option key={name}>{name}</option>
+                  ))}
+                </Select>
+              </label>
+              <SpecialText>{specialText}</SpecialText>
+              <label>
+                <Select
+                  value={outputCurrency}
+                  onChange={(event) => onCurrencyChange(event, "output")}
+                >
+                  {currencyKeys.map((name) => (
+                    <option key={name}>{name}</option>
+                  ))}
+                </Select>
+              </label>
+            </Wrapper>
             <label>
-              <Select
-                value={inputCurrency}
-                onChange={(event) => onCurrencyChange(event, "input")}
-              >
-                {currencyKeys.map((name) => (
-                  <option key={name}>{name}</option>
-                ))}
-              </Select>
+              <Wrapper breakpoint>
+                <span>Kwota do przeliczenia:</span>
+                <Input
+                  type="number"
+                  min="0"
+                  step="0.01"
+                  placeholder="Posiadam.."
+                  required
+                  value={amount ?? ""}
+                  onChange={onAmountChange}
+                />
+                <Mark>{getSymbolFromCurrency(inputCurrency)}.</Mark>
+              </Wrapper>
             </label>
-            <SpecialText>{specialText}</SpecialText>
             <label>
-              <Select
-                value={outputCurrency}
-                onChange={(event) => onCurrencyChange(event, "output")}
-              >
-                {currencyKeys.map((name) => (
-                  <option key={name}>{name}</option>
-                ))}
-              </Select>
+              <Wrapper breakpoint>
+                <span>Kwota po przeliczeniu:</span>
+                <Input
+                  type="number"
+                  placeholder="Otrzymam.."
+                  value={result ? result.toFixed(2) : ""}
+                  readOnly
+                />
+                <Mark>{getSymbolFromCurrency(outputCurrency)}.</Mark>
+              </Wrapper>
             </label>
-          </Wrapper>
+            <Button>Przelicz!</Button>
+          </>
         )}
-        <label>
-          <Wrapper breakpoint>
-            <span>Kwota do przeliczenia:</span>
-            <Input
-              type="number"
-              min="0"
-              step="0.01"
-              placeholder="Posiadam.."
-              required
-              value={amount ?? ""}
-              onChange={onAmountChange}
-            />
-            <Mark>{getSymbolFromCurrency(inputCurrency)}.</Mark>
-          </Wrapper>
-        </label>
-        <label>
-          <Wrapper breakpoint>
-            <span>Kwota po przeliczeniu:</span>
-            <Input
-              type="number"
-              placeholder="Otrzymam.."
-              value={result ? result.toFixed(2) : ""}
-              readOnly
-            />
-            <Mark>{getSymbolFromCurrency(outputCurrency)}.</Mark>
-          </Wrapper>
-        </label>
-        <Button>Przelicz!</Button>
       </Fieldset>
     </StyledForm>
   );
