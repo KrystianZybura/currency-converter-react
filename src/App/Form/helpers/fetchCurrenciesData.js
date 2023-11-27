a9509903561ad825ecb84a854613328fff08f986
Krystian Żybura <Krystian12324444@gmail.com>
2023-11-27 15:26:52 +0100

    short timeout added for loading process

--- src/App/Form/helpers/fetchCurrenciesData.js
+++ src/App/Form/helpers/fetchCurrenciesData.js
@@ -1,6 +1,10 @@
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 const fetchCurrenciesData = () =>
-  fetch(
-    "https://api.currencyapi.com/v3/latest?apikey=cur_live_AxnuY6UVn9CWr0fqPVWHhlCIKaoADn1eF2Dfad6y&base_currency=EUR"
-  ).then((response) => response.json());
+  wait(200).then(() =>
+    fetch(
+      "https://api.currencyapi.com/v3/latest?apikey=cur_live_AxnuY6UVn9CWr0fqPVWHhlCIKaoADn1eF2Dfad6y&base_currency=EUR"
+    ).then((response) => response.json())
+  );
 
 export { fetchCurrenciesData };
