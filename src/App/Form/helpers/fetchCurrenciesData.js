22c27dec5ecea66ce55b37280ecb4b884752f7db
Krystian Żybura <Krystian12324444@gmail.com>
2023-11-28 00:02:06 +0100

    refactored

--- src/App/Form/helpers/fetchCurrenciesData.js
+++ src/App/Form/helpers/fetchCurrenciesData.js
@@ -1,10 +1,11 @@
+const CURRENCIES_API_KEY =
+  "https://api.currencyapi.com/v3/latest?apikey=cur_live_AxnuY6UVn9CWr0fqPVWHhlCIKaoADn1eF2Dfad6y&base_currency=EUR";
+
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const fetchCurrenciesData = () =>
   wait(200).then(() =>
-    fetch(
-      "https://api.currencyapi.com/v3/latest?apikey=cur_live_AxnuY6UVn9CWr0fqPVWHhlCIKaoADn1eF2Dfad6y&base_currency=EUR"
-    ).then((response) => response.json())
+    fetch(CURRENCIES_API_KEY).then((response) => response.json())
   );
 
 export { fetchCurrenciesData };
