9e5900548055cb34891cd0b75d81dadc84002455
Krystian Żybura <Krystian12324444@gmail.com>
2023-11-27 18:04:27 +0100

    default options added, additionally testing react query devtools

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { ThemeProvider } from "styled-components";
 
 import App from "./App";
 import GlobalStyles from "./GlobalStyles";
 import { theme } from "./theme";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5000, // 5 seconds
+      cacheTime: 1000 * 60 * 10, // 10 minutes
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <ThemeProvider theme={theme}>
         <GlobalStyles />
         <App />
